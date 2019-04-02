c85e3e5f89bd6a097d17a399095bb35a3fffedac
Anang Satria <anang.dista.satria@gmail.com>
2019-04-03 04:59:58 +0700

    Updates

---
+++ precache-manifest.8c5f007160c46c8868a22747f2c588d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "64e44e3c60d196b26512",
+    "url": "/robofriends/static/js/main.64e44e3c.chunk.js"
+  },
+  {
+    "revision": "6be626e07f14b889fcaa",
+    "url": "/robofriends/static/js/2.6be626e0.chunk.js"
+  },
+  {
+    "revision": "64e44e3c60d196b26512",
+    "url": "/robofriends/static/css/main.26c54fc7.chunk.css"
+  },
+  {
+    "revision": "6be626e07f14b889fcaa",
+    "url": "/robofriends/static/css/2.36dd252a.chunk.css"
+  },
+  {
+    "revision": "45e7707161e92789e90795f16d624c49",
+    "url": "/robofriends/index.html"
+  }
+];
